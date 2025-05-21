2e3b4aab64803dbc3e25549d0e1031dab44b2a02
Lisseth-11 <lissethguazhambo8@gmail.com>
2025-05-21 10:33:16 -0500

    cambios

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import plotly.express as px 
 
-st.title("My Streamlit App")
-st.header("Welcome to my app!")
-st.write("Hello, world!")
+st.title("Mi segunda publicación")
+st.header("Introducción")
+st.write("This is a simple Streamlit app")
+
+fig = px.bar(x=["A", "B", "C"], y=[1, 3, 2])
+st.plotly_chart(fig)
