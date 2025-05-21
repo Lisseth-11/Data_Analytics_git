341ee13bc2c4d8739f7ddbb24179d04146cfbbd3
Lisseth-11 <lissethguazhambo8@gmail.com>
2025-05-21 10:48:51 -0500

    intento

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import plotly.express as px
 
 st.title("Mi segunda publicación")
 st.header("Introducción")
-st.write("This is a simple Streamlit app")
+st.write("Primera publicación en Streamlit")
 
 fig = px.bar(x=["A", "B", "C"], y=[1, 3, 2])
 st.plotly_chart(fig)
